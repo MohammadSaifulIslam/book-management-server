d1ba8cf26e2be46021bff5ac6b9ef332ac666c6e
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-12 03:08:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgqetuh.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = "mongodb+srv://bookManager:oIqiqX2Y8DcQ2Dw9@cluster0.kgqetuh.mongodb.net/?retryWrites=true&w=majority";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
